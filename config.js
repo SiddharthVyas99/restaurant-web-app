bdcbeeaf315d8b25e06f3cee5b6f4d811ce0d57b
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2021-03-28 21:00:37 +0530

    make it better

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : "mongodb+srv://sid:f4OzrNQRtnuigQXD@cluster0.srsmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+}
+//mongodb+srv://sid:<password>@cluster0.srsmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+//mongodb://localhost:27017/conFusion
