a79eca1ad28c101946054962330a748a1b29b661
Siddharth Vyas <siddharth.vyas9299@gmail.com>
2021-03-29 01:49:30 +0530

    First of all

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use('/users', usersRouter);
 
 
 
-if(process.env.NODE_ENV=="production"){
+if(process.env.NODE_ENV==="production"){
 app.use(express.static('tulip-client/build'));
 const path=require('path');
 app.get("*",(req,res)=>{
